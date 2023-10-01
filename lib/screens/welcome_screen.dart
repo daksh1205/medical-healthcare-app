b991c5bdeb5d478bb37bf5972b62de0acb9800e4
daksh <dakshyt1205@gmail.com>
2023-10-02 02:49:41 +0530

    first commit

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 15,
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {
+                  // Navigator.push(context, MaterialPageRoute(
+                  //builder: (context) => ));
+                },
+                child: Text(
+                  'SKIP',
+                  style: TextStyle(
+                    color: Color(
+                      0xFF7165D6,
+                    ),
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Image.asset("lib/images/doctors.png"),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              "Doctor's Appointment",
+              style: TextStyle(
+                color: Color(
+                  0xFF7165D6,
+                ),
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1,
+                wordSpacing: 2,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Appoint your Doctor",
+              style: TextStyle(
+                color: Colors.black54,
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
